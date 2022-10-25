598859cf6148cc03cc048573f454970927e6c558
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-10-25 17:25:50 +0600

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -8,7 +8,10 @@ const Home = () => {
     return (
         <div>
             <h2> Home:{allCourses.length} </h2>
-           
+            <div style={{ height: '100vh' }}>
+                <img className='img-fluid' src="https://www.keg.com/hubfs/shutterstock_404189197.jpg" alt="" />
+            </div>
+
             {
                 allCourses.map(news => <NewsSummaryCard
                     key={news._id}
