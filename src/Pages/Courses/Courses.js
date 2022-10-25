77b7ce00e84d20feda68663ed42b20b285420510
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-10-25 17:09:41 +0600

    details header option with dwnload icon  added

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -3,12 +3,15 @@ import './Courses.css';
 import { Link, useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { FaGoogle, FaDownload } from "react-icons/fa";
 
 const Courses = () => {
     const news = useLoaderData();
     const { title, details, category_id, image_url } = news;
     return (
+        
         <Card>
+            <h1> <FaDownload></FaDownload> Details Page</h1>
             <Card.Img variant="top" src={image_url} />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
