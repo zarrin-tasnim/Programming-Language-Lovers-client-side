4311a49263764373b68194be353ff4465542add6
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-10-25 10:20:04 +0600

    loader all data  added

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -5,10 +5,9 @@ import Button from 'react-bootstrap/Button';
 
 const News = () => {
     const news = useLoaderData();
-    const { title, details, image_url, category_id } = news;
+    const { title, details, category_id } = news;
     return (
         <Card>
-            <Card.Img variant="top" src={image_url} />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>
